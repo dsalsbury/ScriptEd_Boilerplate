18505e872f5f501bf0fb68d1ca80333a4f8320e8
Diana Salsbury <diana.salsbury@gmail.com>
2016-02-11 21:38:24 +0000

    my first commit

--- _base/static/js/base.js
+++ _base/static/js/base.js
@@ -1,9 +1,3 @@
-var //Any Name for variable
-    =
-  {
-
-  };
-
 //Functions
   function myFunction(x,y) {
       return x+y; // The function returns the sum of x and y
@@ -12,4 +6,8 @@ var //Any Name for variable
 //buttons
 function myButton() {
     document.getElementById("container").innerHTML = "Hello World";
+}
+
+function myButton() {
+    alert("Welcome to the index!");
 }
